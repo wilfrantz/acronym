c18386a05480776b295558f2aa745fffa5cdd3c3
Wilfrantz <wilfrantz@outlook.com>
2019-12-27 19:23:30 +0100

    changed folder name

---
+++ headers/functions.h
@@ -0,0 +1,12 @@
+
+// header guard statements to prevent double inclusion
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+int add (char*, char*);
+int del (char*, char*);
+int search (char*);
+int edit (char*, char*);
+int help (char*);
+
+#endif
