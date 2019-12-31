9abca8751550082ff8fab31ccf5394fb93ecfbc1
Wilfrantz <wilfrantz@outlook.com>
2019-12-31 21:15:01 +0100

    Optimizing code.

--- help.c
+++ help.c
@@ -1,56 +1,73 @@
 
-// Credit to tony/NCURSES-Programming-HOWTO-examples on github.
 // help function brings up the program documentation file to stdout.
 // It uses ncurses to customize the output windown and read from the help
 // file located in the .datfiles acronyms directory 
+// Credit to tony/NCURSES-Programming-HOWTO-examples on github.
 
 #include <ncurses.h>
 #include <stdlib.h>
 
 int help(void){
+
+	// Local variable declaration.
 	int ch, prev, row, col;
 	prev = EOF;
+
+	// file pointer.
 	FILE *fp;
 	int y, x;
 
 	// Openng the file.
 	fp = fopen("/Users/diddy/acronym/.datfiles/help","r");
 
+	// Process condition.
 	if(fp == NULL)
 	{
 		perror("Cannot open help section file");
 		exit(1);
 	}
 
-	initscr();				/* Start curses mode */
-	getmaxyx(stdscr, row, col);		/* find the boundaries of the screeen */
-	while((ch = fgetc(fp)) != EOF)	/* read the file till we reach the end */
-	{
-		getyx(stdscr, y, x);		/* get the current curser position */
-		if(y == (row - 1))			/* are we are at the end of the screen */
-		{
-			printw("<Press Any Key To Exit>");	/* tell the user to press a key to exit */
+	/* Start curses mode */
+	initscr();
+	/* find the boundaries of the screeen */
+	getmaxyx(stdscr, row, col);		
+	/* read the file till we reach the end */
+	while((ch = fgetc(fp)) != EOF){
+		/* get the current curser position */
+		getyx(stdscr, y, x);		
+		/* are we are at the end of the screen */
+		if(y == (row - 1)){
+			/* tell the user to press a key to exit */
+			printw("<Press Any Key To Exit>");	
 			getch();
-			clear();				/* clear the screen */
-			move(0, 0);			/* start at the beginning of the screen */
+			/* clear the screen */
+			clear();				
+			/* start at the beginning of the screen */
+			move(0, 0);			
 		}
-		if(prev == '/' && ch == '*')    	/* If it is / and * then only
-											 * switch bold on */    
-		{
+
+		/* If it is / and * then only * switch bold on */    
+		if(prev == '/' && ch == '*'){
 			attron(A_BOLD);			/* cut bold on */
 			getyx(stdscr, y, x);		/* get the current curser position */
 			move(y, x - 1);			/* back up one space */
 			printw("%c%c", '/', ch); 		/* The actual printing is done here */
-		}
-		else
+		}else{
 			printw("%c", ch);
-		refresh();
+			refresh();
+		}
+
 		if(prev == '*' && ch == '/')
-			attroff(A_BOLD);        		/* Switch it off once we got *
-											 * and then / */
+			/* Switch it off once we got * and then / */
+			attroff(A_BOLD);        		
+
 		prev = ch;
 	}
-	endwin();                       	/* End curses mode */
+
+	// End curses mode 
+	endwin(); 
+	// close the file.
 	fclose(fp);
+
 	return 0;
 }
