4b3cfee05abdb1400cf03b496c60ceb8bc931dcc
Wilfrantz <wilfrantz@outlook.com>
2020-04-14 21:01:12 +0100

    Function optimized

--- add.c
+++ add.c
@@ -1,70 +1,37 @@
 
-
 #include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-#include <unistd.h>
 
 #include "functions.h"
 #include "utilities.h"
 
-/*
- * This function add an acronym to the database,
- * it receives the string with the acronym and 
- * the path to the database.
- * It returns 0 for success and a non-zero for failure! */
-
 int add (char* arg, char* path){
 
-    // path to the acronym file.
-    char* file = strcat(path, arg);
-
-    // local variable
-    char ch;
-    char upper_arg[SIZE];
-    int i = 0;
-
-    // check if the acronym file exist.
-    if (access(file, F_OK) != -1){ // file exists.
-
 	// turn accronym to upper case letters.
-	// set as a convention.
-	while (arg[i]) { 
-	    ch = arg[i]; 
-	    upper_arg[i] = toupper(ch); 
-	    i++; 
+	char* upper_arg = to_upper(arg);
+
+	char* filePath = strcat (path, arg);
+
+	FILE* fp = fopen(filePath, "w+x");
+	if (!fp){
+		// send error message to stdout.
+		perror(upper_arg);
+		exit(EXIT_FAILURE);
+	} else {
+		char* buffer = (char*) malloc(sizeof(char*) * SIZE);
+		
+		// User prompt for acronym definition.
+		printf("\nInsert definition for %s : \n> ", upper_arg);
+		fgets(buffer, sizeof(buffer), stdin);
+
+		// write to acronynm file
+		fprintf(fp, "\n\n\t%s :\n \t\t%s\n\n\n", upper_arg, buffer);
+
+		free(buffer);
 	}
-	// send error message to stdout.
-	printf("\nThe acronym %s exist in the database.", upper_arg);
-	printf("\nRun the program with %s as argument.\n", upper_arg);
-	printf("\nUsage: whts %s\n", upper_arg);
-	exit(3);
-    }else{ // file does not exists
 
-	// turn accronym to upper case letters.
-	// set as a convention.
-	while (arg[i]) { 
-	    ch = arg[i]; 
-	    upper_arg[i] = toupper(ch); 
-	    i++; 
-	}
-	// file pointer variable 
-	FILE* fp = fopen (file, "w");
-	char str[256];
-
-	// User prompt for acronym definition.
-	// stores the information in the array string.
-	printf("\nInsert acronym related info:\n> ");
-	fgets(str, sizeof(str), stdin);
-
-	// write acronym and definition to file in the .datfiles
-	// directory.
-	fprintf(fp, "\n\n\t%s :\n \t\t%s\n\n\n", upper_arg, str);
-
-	//close file
+	// close file
 	fclose(fp);
-    }
-
-    return 0;
+	return 0;
 }
