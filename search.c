1206d9b1c88204eec497cfb520c749029a54929e
Wilfrantz <wilfrantz@outlook.com>
2020-04-14 21:47:18 +0100

    optimization and refactoring

---
+++ search.c
@@ -0,0 +1,38 @@
+
+/* search.c :
+ * The search function seaches the database directoty
+ * for an acrony and use the less system command 
+ * to display the file to stdout
+ * */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "functions.h"
+#include "utilities.h"
+
+int search (char* acro, char* filePath){
+	// Acronym to upper case.
+	const char* upper_acro = to_upper(acro);
+	// path to the file
+	strcat(filePath, acro);
+
+	// Check if the acronym file exist.
+	FILE* fp = fopen(filePath, "r+x");
+	if(!fp)	{
+		perror(upper_acro);
+		exit(EXIT_FAILURE);
+	}
+
+	char* cmd = (char*) malloc(sizeof(char) * SIZE);
+	strcpy(cmd, "less ");
+	strcat(cmd, filePath);
+
+	// execute system command.
+	system(cmd);
+
+	free(cmd);
+
+	exit (0);
+}
