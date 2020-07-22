93716e6ab48880f9a9085f7f498dc789c4145aa5
Wilfrantz DEDE <Dede@MBPX7.local>
2020-07-22 13:31:15 +0100

    Fix typos

--- acronym.c
+++ acronym.c
@@ -23,7 +23,7 @@
 
 int main (int argc, char** argv){
 	// database directory path.
-	char pathDir[] = "/Users/diddy/Code/Cprog/acronym/.datfiles/";
+	char pathDir[] = "/Users/Dede/Code/Cprog/acronym/.datfiles/";
 
 	char* acronym = (char*) malloc(sizeof(char) * SIZE);
 
